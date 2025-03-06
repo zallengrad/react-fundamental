f6b49588f50f845f61ca31b307721977f4d609b7
zallengrad <zallengrad66@gmail.com>
2025-03-06 10:17:30 +0700

    jsx - merender dynamic variabel

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className='App'>
       <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
+
     </div>
   )
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,24 @@
 function Article () {
-    return <div>Ini adalah component pertama , dan kedua</div>;
+    const name = "Zale";
+    const titles = ["Tutorial Reactjs", "Tutorial Nextjs", "Tutorial Nodejs"];
+
+    // titles.map(item => item)
+    return (
+    <>
+        <div>{name}</div>
+        <div>
+            {titles.map((title) => {
+                return (
+                    <>
+                        <div>{title}</div>
+                        <div>{title}</div>
+
+                    </>
+                 )
+            })}
+        </div>
+    </>
+    );
 }
 
 export default Article;
